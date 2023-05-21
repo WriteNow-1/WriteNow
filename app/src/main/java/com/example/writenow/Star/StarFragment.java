bd8b93c5b25d8d3aa10421124975ca7d264a9d90
barcoding11111 <cadisdi77@gmail.com>
2023-05-21 19:34:34 +0900

    StarFragment에서 저장된 대학별로 접근해서 수정,저장가능

--- app/src/main/java/com/example/writenow/Star/StarFragment.java
+++ app/src/main/java/com/example/writenow/Star/StarFragment.java
@@ -1,66 +1,122 @@
 package com.example.writenow.Star;
-
+import com.example.writenow.Write.ResultActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.example.writenow.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StarFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private LinearLayout buttonContainer;
+    private DatabaseReference databaseReference;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StarFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StarFragment newInstance(String param1, String param2) {
-        StarFragment fragment = new StarFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_star, container, false);
+        buttonContainer = view.findViewById(R.id.buttonContainer);
+        databaseReference = FirebaseDatabase.getInstance().getReference("결과");
+
+        createUniversityButtons();
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_star, container, false);
+    private void createUniversityButtons() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot universitySnapshot : snapshot.getChildren()) {
+                    String university = universitySnapshot.getKey();
+                    Button button = createButton(university);
+                    buttonContainer.addView(button);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle database error
+            }
+        });
+    }
+
+    private Button createButton(final String university) {
+        Button button = new Button(requireContext());
+        button.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        button.setText(university);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSubjectButtons(university);
+            }
+        });
+        return button;
+    }
+
+    private void showSubjectButtons(final String university) {
+        buttonContainer.setVisibility(View.GONE);
+        LinearLayout subjectLayout = requireView().findViewById(R.id.subjectLayout);
+        subjectLayout.setVisibility(View.VISIBLE);
+
+        Button subjectButton1 = requireView().findViewById(R.id.subjectButton1);
+        Button subjectButton2 = requireView().findViewById(R.id.subjectButton2);
+        Button freetextButton = requireView().findViewById(R.id.freetextButton);
+
+        subjectButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadResult(university, "1");
+            }
+        });
+
+        subjectButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadResult(university, "2");
+            }
+        });
+
+        freetextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadResult(university, "자율문항");
+            }
+        });
+    }
+
+    private void loadResult(String university, String questionNumber) {
+        DatabaseReference universityRef = databaseReference.child(university).child(questionNumber).child("결과");
+        universityRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String result = snapshot.getValue(String.class);
+                    Intent intent = new Intent(getActivity(), ResultActivity.class);
+                    intent.putExtra("result", result);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle database error
+            }
+        });
     }
 }

a7bd56493e39c50dbdb8c54e0593eec7bb26053b
JunHwi <jubn0433@gmail.com>
2023-06-04 14:54:53 +0900

    홈, set 변경

--- app/src/main/java/com/example/writenow/Home/HomeFragment.java
+++ app/src/main/java/com/example/writenow/Home/HomeFragment.java
@@ -1,21 +1,157 @@
 package com.example.writenow.Home;
 
-import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.writenow.R;
 
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class HomeFragment extends Fragment {
 
+    private String readDay = null;
+    private String str = null;
+    private CalendarView calendarView;
+    private Button cha_Btn, del_Btn, save_Btn;
+    private TextView diaryTextView, textView2, textView3;
+    private EditText contextEditText;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        calendarView = rootView.findViewById(R.id.calendarView);
+        diaryTextView = rootView.findViewById(R.id.diaryTextView);
+        save_Btn = rootView.findViewById(R.id.save_Btn);
+        del_Btn = rootView.findViewById(R.id.del_Btn);
+        cha_Btn = rootView.findViewById(R.id.cha_Btn);
+        textView2 = rootView.findViewById(R.id.textView20);
+        contextEditText = rootView.findViewById(R.id.contextEditText);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                diaryTextView.setVisibility(View.VISIBLE);
+                save_Btn.setVisibility(View.VISIBLE);
+                contextEditText.setVisibility(View.VISIBLE);
+                textView2.setVisibility(View.INVISIBLE);
+                cha_Btn.setVisibility(View.INVISIBLE);
+                del_Btn.setVisibility(View.INVISIBLE);
+                diaryTextView.setText(String.format("%d / %d / %d", year, month + 1, dayOfMonth));
+                contextEditText.setText("");
+                checkDay(year, month, dayOfMonth);
+            }
+        });
+
+        save_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveDiary(readDay);
+                str = contextEditText.getText().toString();
+                textView2.setText(str);
+                save_Btn.setVisibility(View.INVISIBLE);
+                cha_Btn.setVisibility(View.VISIBLE);
+                del_Btn.setVisibility(View.VISIBLE);
+                contextEditText.setVisibility(View.INVISIBLE);
+                textView2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        return rootView;
+    }
+
+    public void checkDay(int cYear, int cMonth, int cDay) {
+        readDay = "" + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt";
+        FileInputStream fis;
+
+        try {
+            fis = getActivity().openFileInput(readDay);
+            byte[] fileData = new byte[fis.available()];
+            fis.read(fileData);
+            fis.close();
+            str = new String(fileData);
+
+            contextEditText.setVisibility(View.INVISIBLE);
+            textView2.setVisibility(View.VISIBLE);
+            textView2.setText(str);
+
+            save_Btn.setVisibility(View.INVISIBLE);
+            cha_Btn.setVisibility(View.VISIBLE);
+            del_Btn.setVisibility(View.VISIBLE);
+
+            cha_Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    contextEditText.setVisibility(View.VISIBLE);
+                    textView2.setVisibility(View.INVISIBLE);
+                    contextEditText.setText(str);
+
+                    save_Btn.setVisibility(View.VISIBLE);
+                    cha_Btn.setVisibility(View.INVISIBLE);
+                    del_Btn.setVisibility(View.INVISIBLE);
+                    textView2.setText(contextEditText.getText());
+                }
+            });
+
+            del_Btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    textView2.setVisibility(View.INVISIBLE);
+                    contextEditText.setText("");
+                    contextEditText.setVisibility(View.VISIBLE);
+                    save_Btn.setVisibility(View.VISIBLE);
+                    cha_Btn.setVisibility(View.INVISIBLE);
+                    del_Btn.setVisibility(View.INVISIBLE);
+                    removeDiary(readDay);
+                }
+            });
+
+            if (textView2.getText() == null) {
+                textView2.setVisibility(View.INVISIBLE);
+                diaryTextView.setVisibility(View.VISIBLE);
+                save_Btn.setVisibility(View.VISIBLE);
+                cha_Btn.setVisibility(View.INVISIBLE);
+                del_Btn.setVisibility(View.INVISIBLE);
+                contextEditText.setVisibility(View.VISIBLE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeDiary(String readDay) {
+        FileOutputStream fos;
+        try {
+            fos = getActivity().openFileOutput(readDay, Context.MODE_PRIVATE);
+            String content = "";
+            fos.write((content).getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveDiary(String readDay) {
+        FileOutputStream fos;
+        try {
+            fos = getActivity().openFileOutput(readDay, Context.MODE_PRIVATE);
+            String content = contextEditText.getText().toString();
+            fos.write((content).getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/writenow/Set/SetFragment.java
+++ app/src/main/java/com/example/writenow/Set/SetFragment.java
@@ -1,5 +1,6 @@
 package com.example.writenow.Set;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.writenow.Login.LoginActivity;
+import com.example.writenow.Login.RegisterActivity;
 import com.example.writenow.R;
 
 
 public class SetFragment extends Fragment {
 
+    Button btnlogout;
+
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set, container, false);
+        view = inflater.inflate(R.layout.fragment_set, container, false);
+
+        btnlogout = view.findViewById(R.id.logout);
+
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
